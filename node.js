e879077cb8d2b5309221ab955d2fb24ee1b85b3a
connieyeeson <connieyeeson@gmail.com>
2023-07-17 16:58:16 +0000

    gameOptions

--- node.js
+++ node.js
@@ -1,13 +1,16 @@
-
+ 
 // Targeted Elements
-let question = document.getElementById('questions');
-let nextButton = document.getElementById("nextBtn");
-let ansBar = document.getElementById("ans");
-let scoreElement = document.querySelector('.point');
-let startQuiz = document.querySelector(".startQuiz");
-let display = document.querySelector(".quesNans-bar");
-let navBar = document.querySelector(".nav-bar");
-console.log(startQuiz);
+const question = document.getElementById('questions');
+const nextButton = document.getElementById("nextBtn");
+const exitsButton = document.getElementById("exitBtn")
+const ansBar = document.getElementById("ans");
+const scoreElement = document.querySelector('.point');
+const startQuiz = document.querySelector(".startQuiz");
+const display = document.querySelector(".quesNans-bar");
+const navBar = document.querySelector(".nav-bar");
+const showQuestions = document.querySelectorAll(".options")
+    console.log(exitsButton)
+    console.log(startQuiz);
 
 
 // Declared variables
@@ -17,39 +20,14 @@ let correctAns;
 let counter = 1;
 let score = 0;
 let questionIn = false;
-
-
-
-
-// Functions
+let currentQuestionIndex = 0;
 
     const quizQuestions = [
 
-        {
-           
-             quetion: "JavaScript is a _________ programming language",
-             multiple: [ "Object-oriented","Functional", "Procedural","All of the above"],
-             correctAns: 3
-        },
-
-        {
-             question: "Which of the following is not a valid JavaScript data type?",
-             Options:["String","Number","Boolean","Character"],
-             correctAns: 3
-        },
-
-        {
-          question: "Which operator is used to concatenate strings in JavaScript?",
-          Options:[ "+","-","*",  "/",],
-          correctAns:0
-
-        },
-
-        {
-
-                question: "Which command is used to create a new directory in Linux?",
-                Options:["cd", "mkdir", "rm", "cp"],
-                correctAns: 1
+         {
+           question: "Which command is used to create a new directory in Linux?",
+           Options:["cd", "mkdir", "rm", "cp"],
+           correctAns: 1
          },
 
          {
@@ -65,22 +43,179 @@ let questionIn = false;
             Options: [ "cat","echo","grep","cut"],
             correctAns: 0
          },
- 
-         // general knowledge
+
+         {
+
+            question: "Who created the Linux kernel?",
+            Options: ["Bill Gates", "Elon Musks", "Linux Torvald", "Kpetermeni Siakor"],
+            correctAns: 2
+
+        },
+       {
+            question: "Which Linux distribution is known for its use in servers and is often used in enterprise environments?",
+            Options: ["Ubuntu", "Fedora", "CentOS", "Dedian"],
+            correctAns:0
+
+       },
+
+       {
+
+            question: "What is the default shell in most Linux distributions?",
+            Options: [ "Bash", "PowerShell", "Zsh", "C shell"],
+            correctAns: 0
+
+       },
+
+       {
+
+            question: "Which command is used to list files and directories in Linux?",
+            Options: [ "ls", "mv", "cd", "rm"],
+            correctAns: 0
+
+
+       },
+
+       {
+            question: "Which command is used to search for files or directories in Linux?",
+            Options: ["find", "grep","locate","search"],
+            correctAns: 0
+
+       }
+      
+     ];
+
+     const jsQuestion = [
+
+         {
+           
+            quetion: "JavaScript is a _________ programming language",
+            Options: [ "Object-oriented","Functional", "Procedural","All of the above"],
+            correctAns: 3
+        },
+
+        {
+            question: "Which of the following is not a valid JavaScript data type?",
+            Options:["String","Number","Boolean","Character"],
+            correctAns: 3
+         },
+
+         {
+            question: "Which operator is used to concatenate strings in JavaScript?",
+            Options:[ "+","-","*",  "/",],
+            correctAns:0
+
+         },
+
+          {
+            question: "Which of the following is a JavaScript keyword used to declare a variable?",
+            options: ["let", "define", "declare", "var"],
+            correctAnswer: 3
+         },
+
+         {
+            question: "Which of the following is a JavaScript keyword used to declare a variable?",
+            options: ["let", "define", "declare", "var"],
+            correctAnswer: 3
+         },
+         {
+            question: "Which of the following is a built-in JavaScript function for displaying messages in a dialog box?",
+            options: ["prompt()", "confirm()", "alert()", "log()"],
+            correctAnswer: 2
+        },
+
+        {
+            question: "How do you access the length of an array in JavaScript?",
+            Option: [ " array.length","array.size", "array.count","array.index"],
+            correctAns:0
+        },
+
+        {
+            question: "Which keyword is used to declare a variable in JavaScript?",
+            correctAns: ["var", "let", "const", "all the above"],
+            correctAns: 3
+        }
+
+       
+
+     ];
+
+    const generalKownledgeQuestion = [
+
+         {
+            question: "Which country is known as the Land of the Midnight Sun?",
+            Options: [" Norway", "Spain", "Finland", "Nigeria"],
+            correctAns: 0
+         },
 
          {
 
             question: "Which country is famous for the Great Wall?",
             Options: [ "Liberia", "Brazil", "China","Italy"],
             correctAns: 2
-        },
+         },
 
-        {
-            question: "Which country is known as the Land of the Midnight Sun?",
-            Options: [" Norway", "Spain", "Finland", "Nigeria"],
+         {
+            question: "Who was the first president of the United States?",
+            Options: ["Thomas Jefferson","George Washington", "Abraham Lincoln", "John F. Kennedy"],
+            correctAns:1
+         },
+
+         {
+
+            question: "In which year did World War II end?",
+            Options: ["1939", "1945", "1941", "1950",],
+            correctAns:1
+         },
+
+         {
+            question: "What is the process of a liquid changing into a gas called?",
+            Option: [ "Melting", "Freezing", "Condensation","Evaporation"],
+            correctAns:3
+         },
+
+         {
+            question: "The formula for calculating force is",
+            Option: [ "F = mc^2", "F = ma", "F = mv", "F = md"],
+            correctAns: 1
+         },
+
+         {
+            question: "The longest river in the world is",
+            Option: ["Amazon River", "Nile River", "Mississippi River","Yangtze River"],
+            correctAns: 1
+         },
+
+         {
+            question: "What is the capital city of Australia?",
+            Option: ["Monrovia", "Lagos", "Paris","Canberra"],
+            correctAns: 3
+         },
+
+         {
+            question: ["The African country known as the Land of a Thousand Hills"],
+            Option: ["Rawanda", "Ghana", "China","Finland"],
             correctAns: 0
-        },
-     ];
+         },
+
+         {
+            question: ["The Berlin Wall fell in: "],
+            Option: ["2000", "1961", "1989", "1971"],
+            correctAns:2
+         }
+    ]
+
+
+    // selectOptions
+
+    showQuestions.forEach(function(question){
+
+      question.addEventListener('click', ()=> {
+            location.href = "index.html";
+            updateQuestion();   
+        })
+
+    })
+
 
     function checkQuestion(word, target) {
         word = word.trim();
@@ -120,7 +255,9 @@ let questionIn = false;
         console.log(quizQuestions[counter]);
         question.innerHTML = `${quizQuestions[counter].question}`
         counter++;
-    
+        const currentQuestion = quizQuestions[currentQuestionIndex]
+            console.log(currentQuestion.question);
+            console.log(currentQuestion.Options)
      }
         
 
@@ -137,7 +274,7 @@ let questionIn = false;
       let secs = mins*60;
       updateTimeDisplay(secs);
 
-     let Countdown = setInterval(function()
+      let Countdown = setInterval(function()
 
             {
 
@@ -166,10 +303,9 @@ let questionIn = false;
 
     }
     }
-    Countdown(5); 
 
+    
     // score
-
     function displayScore(score) {
         scoreElement.textContent = score;
 
@@ -182,7 +318,6 @@ let questionIn = false;
 
      nextButton.addEventListener('click', ()=>{
         if( questionIn === false ) return;
-        // nextButton.setAttribute('disabled');
         updateQuestion();
      });
      ansBar.addEventListener('click', (event)=>{
@@ -194,10 +329,34 @@ let questionIn = false;
         checkQuestion(word, element);
 
      })
+
+    //  START linux QUIZ
+
      startQuiz.addEventListener('click', (event)=>{
         updateQuestion();
+        Countdown(5);
         event.target.style.display ="none";
         display.style.display = "flex";
         navBar.style.display = "flex";
      });
+
+     //  START linux QUIZ
+
+     startQuiz.addEventListener('click', (event)=> {
+
+    //     if( jsQuestion     //  START linux QUIZ
+    //     ==   )
+
+    //  });
+
+    // Quit the game
+    
+    exitsButton.addEventListener('click', (event)=>{
+
+        location.href = "Options.html";
+
+
+    })
+updateQuestion();
+showPreviosQuestion();
 
