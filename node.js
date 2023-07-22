0b680d4b2c0be7ce47d02190239a089b937d439b
connieyeeson <connieyeeson@gmail.com>
2023-07-22 04:57:54 +0000

    Game classes

--- node.js
+++ node.js
@@ -1,28 +1,27 @@
  
 // Targeted Elements
-const question = document.getElementById('questions');
 const nextButton = document.getElementById("nextBtn");
 const exitsButton = document.getElementById("exitBtn")
-const ansBar = document.getElementById("ans");
-const scoreElement = document.querySelector('.point');
+const mainDisplay = document.querySelector('.container');
 const startQuiz = document.querySelector(".startQuiz");
 const display = document.querySelector(".quesNans-bar");
 const navBar = document.querySelector(".nav-bar");
 const showQuestions = document.querySelectorAll(".options")
-    console.log(exitsButton)
-    console.log(startQuiz);
+let quizType;
+
 
 
 // Declared variables
 
 
 let correctAns;
-let counter = 1;
+let counter = 0;
 let score = 0;
 let questionIn = false;
 let currentQuestionIndex = 0;
+let questionsLength = 10;
 
-    const quizQuestions = [
+    const linuxQuestions = [
 
          {
            question: "Which command is used to create a new directory in Linux?",
@@ -54,7 +53,7 @@ let currentQuestionIndex = 0;
        {
             question: "Which Linux distribution is known for its use in servers and is often used in enterprise environments?",
             Options: ["Ubuntu", "Fedora", "CentOS", "Dedian"],
-            correctAns:0
+            correctAns:2
 
        },
 
@@ -80,15 +79,28 @@ let currentQuestionIndex = 0;
             Options: ["find", "grep","locate","search"],
             correctAns: 0
 
+       },
+
+       {
+         question: "Which Linux command is used to delete a directory?",
+         Options: ["rm", "rmdir", "delete", "del"],
+         correctAns: 1
+       },
+
+       {
+         question: "Which command is used to see the manual of a command in Linux?",
+         Options: ["man", "help", "info", "command"],
+         correctAns: 0
        }
+
       
      ];
 
-     const jsQuestion = [
+     const jsQuestions = [
 
          {
            
-            quetion: "JavaScript is a _________ programming language",
+            question: "JavaScript is a _________ programming language",
             Options: [ "Object-oriented","Functional", "Procedural","All of the above"],
             correctAns: 3
         },
@@ -101,45 +113,49 @@ let currentQuestionIndex = 0;
 
          {
             question: "Which operator is used to concatenate strings in JavaScript?",
-            Options:[ "+","-","*",  "/",],
+            Options:[ "+","-","*",  "/"],
             correctAns:0
 
          },
 
           {
             question: "Which of the following is a JavaScript keyword used to declare a variable?",
-            options: ["let", "define", "declare", "var"],
-            correctAnswer: 3
+            Options: ["let", "define", "declare", "var"],
+            correctAns: 3
          },
 
-         {
-            question: "Which of the following is a JavaScript keyword used to declare a variable?",
-            options: ["let", "define", "declare", "var"],
-            correctAnswer: 3
-         },
+       
          {
             question: "Which of the following is a built-in JavaScript function for displaying messages in a dialog box?",
-            options: ["prompt()", "confirm()", "alert()", "log()"],
-            correctAnswer: 2
+            Options: ["prompt()", "confirm()", "alert()", "log()"],
+            correctAns: 2
         },
 
         {
             question: "How do you access the length of an array in JavaScript?",
-            Option: [ " array.length","array.size", "array.count","array.index"],
+            Options: [ " array.length","array.size", "array.count","array.index"],
             correctAns:0
         },
 
         {
             question: "Which keyword is used to declare a variable in JavaScript?",
-            correctAns: ["var", "let", "const", "all the above"],
+            Options: ["var", "let", "const", "all the above"],
             correctAns: 3
-        }
+        },
 
-       
-
+        {
+         question: "Which operator is used to check if two values and their types are equal in JavaScript?",
+         Options: ["===", "==", "!=", "!=="],
+         correctAns: 0
+       },
+       {
+         question: "What does the 'this' keyword refer to in JavaScript?",
+         Options: ["The current function", "The global object", "The parent object", "The current object"],
+         correctAns: 3
+       }
      ];
 
-    const generalKownledgeQuestion = [
+    const generalKownledgeQuestions = [
 
          {
             question: "Which country is known as the Land of the Midnight Sun?",
@@ -163,71 +179,140 @@ let currentQuestionIndex = 0;
          {
 
             question: "In which year did World War II end?",
-            Options: ["1939", "1945", "1941", "1950",],
+            Options: ["1939", "1945", "1941", "1950"],
             correctAns:1
          },
 
          {
             question: "What is the process of a liquid changing into a gas called?",
-            Option: [ "Melting", "Freezing", "Condensation","Evaporation"],
+            Options: [ "Melting", "Freezing", "Condensation","Evaporation"],
             correctAns:3
          },
 
          {
             question: "The formula for calculating force is",
-            Option: [ "F = mc^2", "F = ma", "F = mv", "F = md"],
+            Options: [ "F = mc^2", "F = ma", "F = mv", "F = md"],
             correctAns: 1
          },
 
          {
             question: "The longest river in the world is",
-            Option: ["Amazon River", "Nile River", "Mississippi River","Yangtze River"],
+            Options: ["Amazon River", "Nile River", "Mississippi River","Yangtze River"],
             correctAns: 1
          },
 
          {
             question: "What is the capital city of Australia?",
-            Option: ["Monrovia", "Lagos", "Paris","Canberra"],
+            Options: ["Monrovia", "Lagos", "Paris","Canberra"],
             correctAns: 3
          },
 
          {
-            question: ["The African country known as the Land of a Thousand Hills"],
-            Option: ["Rawanda", "Ghana", "China","Finland"],
+            question: "The African country known as the Land of a Thousand Hills ?",
+            Options: ["Rawanda", "Ghana", "China","Finland"],
             correctAns: 0
          },
 
          {
-            question: ["The Berlin Wall fell in: "],
-            Option: ["2000", "1961", "1989", "1971"],
+            question: "The Berlin Wall fell in",
+            Options: ["2000", "1961", "1989", "1971"],
             correctAns:2
-         }
+         },
+         {
+            question: "Which method is used to remove the last element from an array in JavaScript?",
+            Options: ["splice()", "pop()", "shift()", "delete()"],
+            correctAns: 1
+          }
     ]
 
 
+    function quizStart(){
+      mainDisplay.innerHTML = "";
+      mainDisplay.innerHTML = `
+      
+      <header>
+      <div class="timer" id="time">0</div>
+      <p>QUIZme</p>
+      <div class="point">0</div>
+    </header>
+    <div class="content-wrapper">
+    <button id="start" class="startQuiz" onclick="displayQA(quizType)">START QUIZ</button>
+    </div>
+      `
+    }
+
+    function displayQA(quizname){
+      const wrapper = document.querySelector(".content-wrapper");
+      Countdown(2);
+      wrapper.innerHTML = "";
+      wrapper.innerHTML = `
+      <div class="quesNans-bar">
+      <div class="ques-bar">
+        <p id="questions"></p>
+      </div>
+      <div class="ans-bar" id="ans"></div>
+    </div>
+    
+
+    <div class="nav-bar">
+      <input type="button" value="Exit" id="exitBtn" class="exitBtn"/>
+      <input type="button" value="Next" id="nextBtn" class="nextBtn" />
+    </div>
+      `
+      updateQuestion(quizname);  
+
+    }
+
+    function gameEnd(){
+      mainDisplay.innerHTML = "";
+      mainDisplay.innerHTML = `
+         <div class="endScreen">
+            <p>GameEnd</p>
+            <p class="endScore"> Final Score ${score}</p>
+         </div>
+      `
+    }
     // selectOptions
 
     showQuestions.forEach(function(question){
 
-      question.addEventListener('click', ()=> {
-            location.href = "index.html";
-            updateQuestion();   
-        })
+      question.addEventListener('click', (event)=> {
 
+            if( question.innerText === "Linux"){
+               //  location.href = "index.html";
+               quizType = linuxQuestions;
+               quizStart();
+            }       
+            
+            else if( question.innerText === "General Knowledge"){
+                  quizStart();
+                  quizType = generalKownledgeQuestions
+                  //  location.href = "index.html";
+
+            }  
+
+           else if( question.innerText === "JavaScript"){
+                  quizStart()
+                  quizType = jsQuestions
+               //  location.href = "index.html";
+
+            }  
+             
+        })
     })
 
-
     function checkQuestion(word, target) {
+
         word = word.trim();
         correctAns = correctAns.trim();
-        console.log(correctAns);
+      //   console.log(correctAns);
         if(word === correctAns)
         {
             score = score + 10;
+            displayScore(score);
             // console.log('correct');
             target.classList.add('correct');
 
-            displayScore(score);
             questionIn = true;
         }
         else {
@@ -235,36 +320,56 @@ let currentQuestionIndex = 0;
             target.classList.add('wrong');
             questionIn = true;
         }
+
+        questionsLength--;
+
+        if(questionsLength === 0){
+         gameEnd();
+         return;
+        }
+
         
     }
 
-     function updateQuestion(){
+        // update linux Question;
+
+     function updateQuestion(quizname){
+      let ansBar = document.getElementById("ans");
+      let question = document.getElementById('questions');
+
+
         questionIn= false;
-        let correctIndex = quizQuestions[counter].correctAns;
-        ansBar.innerHTML = "";
-        let length = quizQuestions[counter].Options.length;
+        let correctIndex = quizname[currentQuestionIndex].correctAns;
+        ansBar.innerHTML = ""
+        counter++;
+        let length = 4;
         for (let i = 0; i < length; i++){
 
         let answer = document.createElement("p");
         answer.setAttribute("class", "choice");
-        answer.innerHTML = ` ${quizQuestions[counter].Options[i]}`
+        answer.innerHTML = quizname[currentQuestionIndex].Options[i];
         ansBar.appendChild(answer);
-        correctAns = quizQuestions[counter].Options[correctIndex];
+        correctAns = quizname[currentQuestionIndex].Options[correctIndex];
         } 
-        console.log(length);
-        console.log(quizQuestions[counter]);
-        question.innerHTML = `${quizQuestions[counter].question}`
-        counter++;
-        const currentQuestion = quizQuestions[currentQuestionIndex]
+
+
+      //   console.log(length);
+      //   console.log(quizname[counter]);
+        question.innerHTML = quizname[currentQuestionIndex].question;
+        currentQuestionIndex++
+        const currentQuestion = quizname[currentQuestionIndex]
             console.log(currentQuestion.question);
             console.log(currentQuestion.Options)
      }
         
 
-    // updateQuestion();
+     
+        
+     function displayScore(score){
+      let scoreElement = document.querySelector('.point');
+      scoreElement.textContent = score;
 
-
-
+     }
 
         // time
       function Countdown(mins)
@@ -305,57 +410,21 @@ let currentQuestionIndex = 0;
     }
 
     
-    // score
-    function displayScore(score) {
-        scoreElement.textContent = score;
 
-      }
-
-      displayScore(score);
-
-
-      // Event Handlers
-
-     nextButton.addEventListener('click', ()=>{
-        if( questionIn === false ) return;
-        updateQuestion();
-     });
-     ansBar.addEventListener('click', (event)=>{
-        if ( questionIn === true ) return;
-        if(!event.target.classList.contains('choice')) return;
-
-        let word = event.target.textContent;
-        let element = event.target;
-        checkQuestion(word, element);
-
-     })
 
     //  START linux QUIZ
+mainDisplay.addEventListener('click', (event)=> {
+  if(event.target.classList.contains('choice')){
+   if ( questionIn === true ) return;
+      if(!event.target.classList.contains('choice')) return;
 
-     startQuiz.addEventListener('click', (event)=>{
-        updateQuestion();
-        Countdown(5);
-        event.target.style.display ="none";
-        display.style.display = "flex";
-        navBar.style.display = "flex";
-     });
-
-     //  START linux QUIZ
-
-    //  startQuiz.addEventListener('click', (event)=> {
-
-    //     if( jsQuestion ==   )
-
-    //  });
-
-    // Quit the game
-    
-    exitsButton.addEventListener('click', (event)=>{
-
-        location.href = "Options.html";
-
-
-    })
-updateQuestion();
-showPreviosQuestion();
+      let word = event.target.textContent;
+      let element = event.target;
+      checkQuestion(word, element);
+  }
+  else if(event.target.classList.contains('nextBtn')){
+         if( questionIn === false ) return;
+         updateQuestion(quizType);
+  }
+})
 
